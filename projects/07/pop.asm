0d80b10c2d691d91ad90711c03e970131713a9f9
Gray-lab <101153273+Gray-lab@users.noreply.github.com>
2022-08-21 17:28:54 -0500

    finished basic memory test

--- projects/07/pop.asm
+++ projects/07/pop.asm
@@ -1,7 +1,19 @@
 // test of pop mechanism
 
-@0 // SP
-AM=M-1 // SP--
+// get index
+@20
+D=A
+// get pointer of segment and add index
+@LCL
+D=D+M
+// store pointer in a temp register R13
+@R13
+M=D
+// get value in stack
+@SP
+AM=M-1
 D=M
-@10 // seg + index
+// save to location that R13 points to
+@R13
+A=M
 M=D
